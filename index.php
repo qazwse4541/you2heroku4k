569d72ea49f4c06ccb6c226b7da19bee57a9e8e5
qazwse4541 <34997077+qazwse4541@users.noreply.github.com>
2018-08-30 22:46:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,11 @@ ini_set('display_errors', '0');
 //把下面的资料改成您的
 
 //网站名字
-$siteName='DouDouTube';
+$siteName='Youtube';
 
 //youtube API V3 KEY:
 
-$key='AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c';
+$key='AIzaSyBW703DuikAo699uyCt2_UdVMQou7lrGHs';
 
 
 //其他地方不用改。
